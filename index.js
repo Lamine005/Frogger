8436d361abfd1916f3c7752ec1201664bf2e0255
Lamine005 <133173949+Lamine005@users.noreply.github.com>
2023-12-05 23:19:15 -0500

    method movelogright added

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const timeLeftdispaly = document.querySelector('#time-left')
+const timeLeftDisplay = document.querySelector('#time-left')
 const resultDisplay = document.querySelector('#result')
 const startPauseButton = document.querySelector('#start-pause-button')
 const squares = document.querySelectorAll('.grid div')
@@ -7,37 +7,98 @@ const logsRight = document.querySelectorAll('.log-right')
 const carsLeft = document.querySelectorAll('.car-left')
 const carsRight = document.querySelectorAll('.car-right')
 
+let currentIndex = 76;
+const width = 9;
+let timerId;
+let outcomeTimerId;
+let currentTime = 20;
 
+function moveFrog(e) {
+  squares[currentIndex].classList.remove("frog");
 
-let currentIndex = 76
-const width = 9
-let timerId
-let outcomeTimerId
-let currentTime = 20
+  switch (e.key) {
+    case "ArrowRight":
+      if (currentIndex % width < width - 1) currentIndex += 1;
 
+      break;
+    case "ArrowLeft":
+      if (currentIndex % width !== 0) currentIndex -= 1;
 
-function moveFrog(e){
-
-    squares[currentIndex].classList.remove('frog')
-
-    switch(e.key){
-    
-        case 'ArrowRight':
-            if(currentIndex % width < width - 1) currentIndex += 1
-            
-            break
-        case 'ArrowLeft':
-            if(currentIndex % width !==0) currentIndex -= 1
-                
-            break;
-        case 'ArrowDown':
-            if(currentIndex + width < width*width)currentIndex += width
-            break;
-        case 'ArrowUp':
-            if(currentIndex - width >= 0) currentIndex -= width
-            break;        
-    }
-    squares[currentIndex].classList.add('frog')
+      break;
+    case "ArrowDown":
+      if (currentIndex + width < width * width) currentIndex += width;
+      break;
+    case "ArrowUp":
+      if (currentIndex - width >= 0) currentIndex -= width;
+      break;
+  }
+  squares[currentIndex].classList.add("frog");
 }
 
-document.addEventListener('keyup', moveFrog)
+document.addEventListener("keyup", moveFrog);
+
+function autoMoveLogs() {
+  logsLeft.forEach((logLeft) => moveLogLeft(logLeft));
+  logsRight.forEach((logRight) => moveLogright(logRight));
+}
+
+
+function moveLogLeft(logLeft) {
+  switch (true) {
+    case logLeft.classList.contains("l1"):
+      logLeft.classList.remove("l1");
+      logLeft.classList.add("l2");
+      break;
+
+    case logLeft.classList.contains("l2"):
+      logLeft.classList.remove("l2");
+      logLeft.classList.add("l3");
+      break;
+
+    case logLeft.classList.contains("l3"):
+      logLeft.classList.remove("l3");
+      logLeft.classList.add("l4");
+      break;
+
+    case logLeft.classList.contains("l4"):
+      logLeft.classList.remove("l4");
+      logLeft.classList.add("l5");
+      break;
+
+    case logLeft.classList.contains("l5"):
+      logLeft.classList.remove("l5");
+      logLeft.classList.add("l1");
+      break;
+  }
+}
+
+function moveLogright(logRight) {
+  switch (true) {
+    case logRight.classList.contains("l1"):
+      logRight.classList.remove("l1");
+      logRight.classList.add("l5");
+      break;
+
+    case logRight.classList.contains("l2"):
+      logRight.classList.remove("l2");
+      logRight.classList.add("l1");
+      break;
+
+    case logRight.classList.contains("l3"):
+      logRight.classList.remove("l3");
+      logRight.classList.add("l2");
+      break;
+
+    case logRight.classList.contains("l4"):
+      logRight.classList.remove("l4");
+      logRight.classList.add("l3");
+      break;
+
+    case logRight.classList.contains("l5"):
+      logRight.classList.remove("l5");
+      logRight.classList.add("l4");
+      break;
+  }
+}
+
+setInterval(autoMoveLogs, 1000);
